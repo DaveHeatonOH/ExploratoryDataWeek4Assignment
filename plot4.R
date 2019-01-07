f95bbd6ba8f7167ce82a5d02a6aae42b5f0b26df
DaveyH1 <dave_heaton1@hotmail.com>
2019-01-07 20:27:46 +0000

    Plot 4 initial workings

--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 ## Set Working Directory
-setwd("C:\Users\dave_\OneDrive\GitHub\Coursera\Exploratory Data Analysis\ExploratoryDataWeek4Assignment")
+setwd("C:/Users/dave_/OneDrive/GitHub/Coursera/Exploratory Data Analysis/ExploratoryDataWeek4Assignment")
 
 ## Unzip file
 
@@ -10,3 +10,21 @@ unzip(zipF) # Unzip it in working directoy
 ## Check file names in working directory
 dir()
 
+## Load the GGPLOT2 Library
+library(ggplot2)
+
+## Read in the two tables
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Get a list of the SCC values which contain Coal (using level 3)
+coalData <- SCC[grep("Coal", SCC$SCC.Level.Three, ignore.case = T),]
+
+## Create a subset of the data which is limited to just coal based sources
+NEICoal <- subset(NEI, NEI$SCC %in% coalData$SCC)
+
+# Get the years
+years <- unique(NEICoal$year)
+
+ggplot(NEICoal, aes(x = year, y = Emissions)) +
+        geom_point()
