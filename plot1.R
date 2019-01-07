6134dd5460225a713da26b47658377dea0a7e8b8
DaveyH1 <44966177+DaveyH1@users.noreply.github.com>
2019-01-07 14:23:44 +0000

    Initial Commit
    First set of files uploaded to be able to work again from home

---
+++ plot1.R
@@ -0,0 +1,32 @@
+## Set Working Directory
+setwd("~/Coursera/Exploratory Data Analysis/Week 4 Assignments")
+
+## Unzip file
+
+# Create variable to hold information about the file to be unzipped
+zipF <- paste(getwd(),"exdata_data_NEI_data.zip",sep = "/")
+unzip(zipF) # Unzip it in working directoy
+
+## Check file names in working directory
+dir()
+
+## Read in the two tables
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Sum up the total emissions for each individual year to plot on a chart
+polYear <- with(NEI, tapply(Emissions, year, sum, na.rm = T))
+
+## Create a variable for each of the years to plot on the chart
+years <- unique(NEI$year)
+
+## Create a PNG
+png("plot1.png", width = 867, height = 558, units = "px")
+
+## Plot the chart and add a linear regression line to show the trend
+plot(years, polYear, pch = 20, cex = 2, main = "Total Emissions By Year", xlab = "Year", ylab = "Total Emissions (tons)")
+abline(lm(polYear ~ years), col = "red", lwd = 2)
+
+## Close the PNG device
+
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,35 @@
+## Set Working Directory
+setwd("~/Coursera/Exploratory Data Analysis/Week 4 Assignments")
+
+## Unzip file
+
+# Create variable to hold information about the file to be unzipped
+zipF <- paste(getwd(),"exdata_data_NEI_data.zip",sep = "/")
+unzip(zipF) # Unzip it in working directoy
+
+## Check file names in working directory
+dir()
+
+## Read in the two tables
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Subset to just Baltimore
+blt <- subset(NEI, fips == "24510")
+
+## Sum up the total emissions for each individual year to plot on a chart
+polYear <- with(blt, tapply(Emissions, year, sum, na.rm = T))
+
+## Create a variable for each of the years to plot on the chart
+years <- unique(bl$year)
+
+## Create a PNG
+png("plot2.png", width = 867, height = 558, units = "px")
+
+## Plot the chart and add a linear regression line to show the trend
+plot(years, polYear, pch = 20, cex = 2, main = "Total Emissions By Year for Baltimore Area (24510)", xlab = "Year", ylab = "Total Emissions (tons)")
+abline(lm(polYear ~ years), col = "red", lwd = 2)
+
+## Close the PNG device
+
+dev.off()
